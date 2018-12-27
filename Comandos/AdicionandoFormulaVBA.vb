2593403da56e383fa389e4dc031f8b598640a662
Souza, Ricardo <34768535+ricardopssouza1@users.noreply.github.com>
2018-12-27 15:21:38 -0200

    20181227

---
+++ Comandos/AdicionandoFormulaVBA.vb
@@ -0,0 +1,36 @@
+ ActiveSheet(1)
+
+        strSQL = "" _
+                & " SELECT   " _
+                & "   1 AS COL_A  " _
+                & " , 4 AS COL_B  "
+
+        rs = cnn.Execute(strSQL)
+
+        xlApp.DisplayAlerts = False
+
+        'grava dados do select no excel
+        With xlWorkSheet.Range("A2")
+            .CopyFromRecordset(rs)
+        End With
+
+        Rows = xlWorkSheet.UsedRange.Rows.Count
+
+        With xlWorkSheet.Range("A1:C1")
+            .Font.Bold = True
+        End With
+
+        With xlWorkSheet.Range("A1:C" & Rows)
+            .Font.Name = "Calibri"
+            .Font.Size = 10
+            .Font.Color = RGB(0, 0, 0)
+        End With
+
+        ' adicionando formula
+        xlWorkSheet.Range("C2").Formula = "=SOMA(A2+B2)"
+
+
+        'AJUSTES FINAIS AUTOFIT
+        xlWorkSheet.Cells.EntireColumn.AutoFit()
+        xlWorkSheet.Cells.EntireRow.AutoFit()
+        xlWorkSheet.Range("A1").Select()
