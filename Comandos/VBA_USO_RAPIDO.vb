51a377bf332d4e578f8d66dd4480290db8941d45
Souza, Ricardo <34768535+ricardopssouza1@users.noreply.github.com>
2019-05-21 09:31:57 -0300

    20190521

--- Comandos/VBA_USO_RAPIDO.vb
+++ Comandos/VBA_USO_RAPIDO.vb
@@ -356,5 +356,30 @@ Sub Find()
     End If
 End Sub
 
+-- ========================
+-- GRAVANDO NO ARQUIVO TXT
+-- ========================
+        Dim strSQL As String
+        Dim cnn As New ADODB.Connection
+        Dim rs As ADODB.Recordset
+        Dim txt As String
+        Dim sw As StreamWriter
+        Dim versaoSQL As String
+        Dim retorno As String
+ 
+        'Setando a versão do SQL que está sendo utilizada
+        cnn.Open(Dts.Variables("ConexaoString").Value.ToString())
+        strSQL = "select right(left(@@VERSION,25),4)"
+        rs = cnn.Execute(strSQL)
+        versaoSQL = rs.Fields(0).Value.ToString()
 
-			
+        txt = Dts.Variables("CaminhoArquivo").Value.ToString() + "\ARQUIVO.TXT"
+        sw = My.Computer.FileSystem.OpenTextFileWriter(txt, True)
+	
+	'Gravando o resultado no txt
+        sw.WriteLine(versaoSQL)
+        sw.Close()
+
+        'fecha conexão
+        rs.Close()
+        cnn.Close()		
